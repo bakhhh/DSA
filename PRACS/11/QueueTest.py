419f33ecbf8116ad1c678aa5e812975fb724edde
Sohail Bakhshi <112333400+bakhhh@users.noreply.github.com>
2022-12-01 11:18:07 +0800

    Add files via upload

---
+++ PRACS/11/QueueTest.py
@@ -0,0 +1,79 @@
+# Sohail Bakhshi 
+# Student ID - 20605126
+import unittest
+from DSAQueue import *
+
+capacity =100
+class TestQueue(unittest.TestCase):
+
+    def testIsEmpty(self):
+        queue = DSAQueue()
+        self.assertEqual(queue.isEmpty(), True)
+        self.assertEqual(queue.getCount(), 0)
+        for i in range(1, capacity):
+            queue.enqueue(i)
+            self.assertEqual(queue.isEmpty(), False)
+            
+    
+    def testIsFull(self):
+        queue = DSAQueue()
+        for i in range(1, capacity):
+            queue.enqueue(i)
+            if i > capacity:
+                self.assertEqual(queue.isFull(), True)
+            else:
+                self.assertEqual(queue.isFull(), False)
+
+    def testPeek(self):
+        queue = DSAQueue()
+        queue.enqueue(2)
+        queue.enqueue(4)
+        queue.enqueue(5)
+        queue.enqueue(6)
+        self.assertEqual(queue.isEmpty(), False)
+        self.assertEqual(queue.peek(), 2)
+        queue.dequeue()
+        self.assertEqual(queue.peek(), 4)
+        queue.dequeue()
+        self.assertEqual(queue.peek(), 5)
+        queue.dequeue()
+        self.assertEqual(queue.peek(), 6)
+        queue.dequeue()
+        self.assertEqual(queue.isEmpty(), True)
+    
+        
+    def testCount(self):
+        queue = DSAQueue()
+        for i in range(10):
+            queue.enqueue(i+1)
+        self.assertEqual(queue.getCount(), 10)
+        self.assertEqual(queue.dequeue(), 1)
+        self.assertEqual(queue.getCount(), 9)
+        self.assertEqual(queue.dequeue(), 2)
+        self.assertEqual(queue.getCount(), 8)
+   
+    def testEnqueue(self):
+        queue = DSAQueue()
+        for i in range(capacity):
+            queue.enqueue(i)
+
+    def testDequeue(self):
+        queue = DSAQueue()
+        queue.enqueue(1)
+        queue.enqueue(9)
+        queue.enqueue(3)
+        queue.enqueue(5)
+        queue.enqueue(10)
+        self.assertEqual(queue.dequeue(), 1)
+        self.assertEqual(queue.dequeue(), 9)
+        self.assertEqual(queue.dequeue(), 3)
+        self.assertEqual(queue.dequeue(), 5)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
